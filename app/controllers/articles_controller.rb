0e5b9e19591c3cf9b18c6911a72c242b5e9c1fd4
bgdnvk <bnovykov@gmail.com>
2022-07-30 17:24:01 +0200

    new article and validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,5 +6,27 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  # TODO: https://guides.rubyonrails.org/getting_started.html#creating-a-new-article
+
+  # The new action instantiates a new article, but does not save it
+  # This article will be used in the view when building the form. 
+  # By default, the new action will render app/views/articles/new.html.erb, which we will create next.
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
 end
